1805915e232e469df60834a509ea0eec70f64d79
rafaelmnf <rafaelnfilho@gmail.com>
2026-04-10 14:23:38 -0300

    usando Jasmine como test framework

---
+++ Amazon/test-jasmine/moneyTests.js
@@ -0,0 +1,19 @@
+import { formatCurrency } from "../js/utils/util.js";
+
+// Test Suit
+// 'describe' and 'it' are functions of Jasmine
+describe('test suite: formatCurrency', () => {
+    it('converts cents int dollars', () => {
+        expect(formatCurrency(2095)).toEqual('20.95');     
+    })
+
+    it('works with 0', () => {
+        expect(formatCurrency(0)).toEqual('0.00');
+    })
+
+    it('rounds up to the nearest cent', () => {
+        expect(formatCurrency(2000.5)).toEqual('20.01');
+    })
+}) // Function name test --> what test does --> real test
+
+    
