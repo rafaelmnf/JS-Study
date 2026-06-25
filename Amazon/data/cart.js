c792aed51b28db0b091a6f62f2b88f9a268c900d
rafaelmnf <rafaelnfilho@gmail.com>
2026-06-25 15:20:03 -0300

    aprendendo Promises

--- Amazon/data/cart.js
+++ Amazon/data/cart.js
@@ -84,3 +84,15 @@ export function updateDeliveryOption(productId, deliveryOptionId) {
     matchingItem.deliveryOptionId = deliveryOptionId;
     saveToStorage();
 }
+
+export function loadCart(func) {
+  const xhr = new XMLHttpRequest();
+
+  xhr.addEventListener('load', () => {
+    console.log(xhr.response);
+    func();
+  })
+
+  xhr.open('GET', 'https://supersimplebackend.dev/cart');
+  xhr.send();
+}
--- Amazon/js/checkout.js
+++ Amazon/js/checkout.js
@@ -1,10 +1,70 @@
 import { renderOrderSummary } from './checkout/orderSummary.js';
 import { renderPaymentSummary } from './checkout/paymentSummary.js';
 import { loadProducts } from '../data/products.js';
+import { loadCart } from '../data/cart.js';
 // import '../data/cart-class.js'; // it runs all the code inside this file, without importing anithing 
 // import '../data/backend-practice.js';
 
-loadProducts(() => {
+// Promises: Let us wait for some code to finish, before going to the next step
+// When we create this promise it will run the function inside immediately
+new Promise((resolve) => { // resolve is a function that let us control when to go to next step
+    console.log('start promise')
+    loadProducts(() => {
+        console.log('finished loading')
+        resolve(); // Quando carregar os produtos, ai roda a callback function, que vai para o .then dps
+        // resolve controla quando ir para o then
+    })
+}).then(() => {
+    console.log('next step');
+})
+
+// Promise.all: lets us run multiple promises at the same time and wait for all to finish
+Promise.all([
+    new Promise((resolve) => {
+        loadProducts(() => {
+            resolve('value1'); // podemos passar valores entre os resolves
+        });
+    }), 
+    new Promise((resolve) => {
+        loadCart(() => {
+            resolve();
+        })
+    })
+]).then((values) => { 
+    console.log(values);
     renderOrderSummary();
     renderPaymentSummary();
 });
+
+
+/**
+    // Its like organizing the code by steps 
+    new Promise((resolve) => {
+        loadProducts(() => {
+            resolve('value1'); // podemos passar valores entre os resolves
+        });
+
+    }).then((value) => {
+        return new Promise((resolve) => {
+            loadCart(() => {
+                resolve();
+            })
+        }); 
+
+    }).then(() => {
+        renderOrderSummary();
+        renderPaymentSummary();
+    })
+ */
+
+
+/* Using Callbacks starts to produce bad code, as it results in nest inside nest
+    loadProducts(() => {
+        loadCart(() => {
+            renderOrderSummary();
+            renderPaymentSummary();
+        })
+        
+    });
+*/
+
